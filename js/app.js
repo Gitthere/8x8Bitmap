2d4d199ed3bd43b79705939922acea28777206a6
Gitthere <ichi4g@gmail.com>
2014-08-14 18:49:58 -1000

    plots x's to grid

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+var hexInput = "18 3C 7E 7E 18 18 18 18";
+var changeHexInputToArray = hexInput.split(" ");
+console.log(changeHexInputToArray);
+for (i = 0; i < changeHexInputToArray.length; i++) {
+
+  var hexToDecimal = parseInt(changeHexInputToArray[i], 16);
+  var decimalToBinary = hexToDecimal.toString(2);
+  // console.log(decimalToBinary);
+
+  //changes string 11000 to array [ '1', '1', '0', '0', '0' ]
+  var binaryToIndividualElements = decimalToBinary.split("");
+  // console.log(binaryToIndividualElements);
+
+  //change "1" to "x"
+  for (k = 0; k < binaryToIndividualElements.length; k++) {
+    if (binaryToIndividualElements[k] > 0) {
+      binaryToIndividualElements.splice(k, 1, 'x');
+    };
+  };
+
+  var numberOfZerosToConcat = 8 - decimalToBinary.length;
+  // console.log(numberOfZerosToConcat);
+
+  for (j = 0; j < numberOfZerosToConcat; j++) {
+    binaryToIndividualElements.unshift('0');
+  };
+  console.log(binaryToIndividualElements);
+};
